711df73b5a92b500d80d4041c00c32c75a7e5027
Alex Bondarenko <alexandr96b@gmail.com>
2019-06-26 11:14:40 +0300

    add fix

--- js/script.js
+++ js/script.js
@@ -3,13 +3,50 @@ $(document).ready(function () {
 $('.btn-menu').click(function(){
   $('.b-menu').toggleClass('active');
   $('.btn-menu').toggleClass('active');
+  if ($('.btn-menu').hasClass('active')) {
+    $('.btn-menu').text('Закрыть');
+  } else{
+    $('.btn-menu').text('Меню');
+  }  
 });
+
+
+        $('.b-map-button').click(function(){
+          $('.b-map').toggleClass('active');
+          $(this).toggleClass('active');
+
+          });
 
 
 $('.b-menu-xs strong').click(function(){
   $(this).next().slideToggle('active');
   $(this).toggleClass('active');
 });
+
+$(".b-input-tel").inputmask("+7(999)999-99-99");
+
+ 
+/*
+$('.b-block-left__wrap input[type=checkbox]').click(function() {
+    if($(this).not(':checked')) {
+      $('.b-block-left__bottom').addClass('active');
+    }
+    else {
+      $('.b-block-left__bottom').removeClass('active');
+    }
+});
+*/
+
+$(document).ready(function() {
+        $(".b-block-left__wrap input[type=checkbox]").click(function() {
+            var checked = $(this).is(':checked');
+            if (checked) {
+                $('.b-block-left__bottom').addClass('active');
+            } else {
+                $('.b-block-left__bottom').removeClass('active');
+            }
+        });
+    });
 
 // google maps
 
@@ -57,6 +94,8 @@ function init() {
 
 }
 
+
+
 $('.b-store-item__left').click(function(){
   $(this).next().slideToggle('active');
   $(this).toggleClass('active');
@@ -64,14 +103,98 @@ $('.b-store-item__left').click(function(){
 
 
 
+$(".b-aside-bottom").on("click","button", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+ 
+ 
 
- 
- 
+var scrolledpx = parseInt($(window).scrollTop());
+$(window).scroll( function (){
+
+    scrolledpx = parseInt($(window).scrollTop()); 
+
+    if(scrolledpx > 100){
+        $('.b-header').addClass('active');
+     } else if(scrolledpx < 101){
+      $('.b-header').removeClass('active');
+     }
+
+  });
+
+
+
+
+
+$('.b-main-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  fade: false,
+  variableWidth: true,
+  arrows: false,
+  responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 1,
+      variableWidth: false,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+       variableWidth: false,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+
+$('.b-team-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: false,
+  variableWidth: false,
+  arrows: true
+});
+
+
+
+function blogtabs(idblogtabs) {
+var tabs = $(idblogtabs);
+$('.blogtabs4 > .blogtabs5', tabs).each(function(i) {if ( i != 0 ) $(this).hide(0); }); 
+tabs.on('click', '.blogtabs1 a', function(e){
+e.preventDefault();                     
+var tabId = $(this).attr('href');       
+$('.blogtabs1 a',tabs).removeClass();          
+$(this).addClass('blogtabs3active');    
+$('.blogtabs4 > .blogtabs5', tabs).hide(0);
+$(tabId).fadeIn(500);
+}); }
+
+$(document).ready(function(){
+blogtabs('.mytabs1');  
+blogtabs('.mytabs2');   
+});
+
+
 
 
 
 
 });
-
-
-
